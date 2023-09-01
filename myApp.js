1b8fca76dd4356b0b9b8382c2e29ae590d6bf60e
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-09-01 23:13:07 +0300

    Use model.findById() to Search Your Database By _id

--- myApp.js
+++ myApp.js
@@ -64,7 +64,15 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, function(err,data){
+    if(err){
+      console.log(err);
+     }
+     else{
+      done(null , data);
+     }
+  })
+
 };
 
 const findEditThenSave = (personId, done) => {
